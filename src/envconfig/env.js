5fb6fe81f51abadb4ff009e5da8ffe167143a14f
dillonl <dillonl49@163.com>
2021-12-13 12:12:23 +0800

    fix: # :art: 修改部署环境的接口地址

--- src/envconfig/env.js
+++ src/envconfig/env.js
@@ -4,7 +4,7 @@ if (process.env.NODE_ENV === 'development') {
     baseUrl = '//expro.dillonl.com'
     imgUrl = ''
 } else if (process.env.NODE_ENV === 'production') {
-    baseUrl = ''
+    baseUrl = '//expro.dillonl.com'
     imgUrl = ''
 }
 export {
