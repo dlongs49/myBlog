e468a89bd277aaf0d0f4caa6443d563464604419
dillonl <dillonl49@163.com>
2022-10-12 14:35:51 +0800

    fix:# 修改已知bug

--- public/u/js/medium.js
+++ public/u/js/medium.js
@@ -1,6 +1,6 @@
 $(function() {
-    var pc = '<link rel="stylesheet" type="text/css" href="//www.dillonl.com/static/index.css" />'
-    var mobile = '<link rel="stylesheet" type="text/css" href="//www.dillonl.com/static/mobile.css" />'
+    var pc = '<link rel="stylesheet" type="text/css" href="/u/css/index.css" />'
+    var mobile = '<link rel="stylesheet" type="text/css" href="/u/css/mobile.css" />'
     if (navigator.userAgent.match(/IEMobile|BlackBerry|Android|iPod|iPhone|iPad/i)) {
         $("head").append(mobile)
     } else {
