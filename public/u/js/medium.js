478e96865d5e6d4c64eacb72520bca4df827741b
dillonl <dillonl49@163.com>
2022-10-30 16:39:51 +0800

    chore:# 🛵 构建媒体查询

--- public/u/js/medium.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    var pc = '<link rel="stylesheet" type="text/css" href="/u/css/index.css" />'
-    var mobile = '<link rel="stylesheet" type="text/css" href="/u/css/mobile.css" />'
-    if (navigator.userAgent.match(/IEMobile|BlackBerry|Android|iPod|iPhone|iPad/i)) {
-        $("head").append(mobile)
-    } else {
-        $("head").append(pc)
-    }
-})
--- src/views/home/home.jsx
+++ src/views/home/home.jsx
@@ -17,17 +17,17 @@ class Home extends Component {
 
     }
     qqClick() {
-        let appid = '101936013'
-        let recturl = encodeURIComponent('http://expro.dillonl.com/api/v2/qqlogin')
-        let scope = '0'
-        let display = null; // 默认pc 'mobile' 移动端
+        const appid = '101936013'
+        const recturl = encodeURIComponent('http://expro.dillonl.com/api/v2/qqlogin')
+        const scope = '0'
+        const display = null; // 默认pc 'mobile' 移动端
         window.location.href =
             `https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id=${appid}&redirect_uri=${recturl}&scope=${scope}&display=${display}`
 
     }
     wbClick() {
-        let appid = '1883180365'
-        let recturl = encodeURIComponent('http://expro.dillonl.com/api/v2/wblogin')
+        const appid = '1883180365'
+        const recturl = encodeURIComponent('http://expro.dillonl.com/api/v2/wblogin')
         window.location.href =
             `https://api.weibo.com/oauth2/authorize?client_id=${appid}&response_type=code&redirect_uri=${recturl}`
 
@@ -44,7 +44,7 @@ class Home extends Component {
         const item = this.state.item
         return (
             <div className="content">
-                {/* <div className="third">
+                 {/* <div className="third">
                     <a onClick={this.qqClick} className="alog qqlogin">
                         <i className="icon i_qq"></i>
                         <span className="spn">QQ</span>
@@ -53,10 +53,10 @@ class Home extends Component {
                         <i className="icon i_wb"></i>
                         <span className="spn">微博</span>
                     </a>
-                </div> */}
+                </div>  */}
                 <canvas className="snow" id="snow" width="1349" height="400"></canvas>
                 <div className="main-text">
-                    <h1 onClick={this.handleVerce.bind(this)} className="ram">{item.verse_title}</h1>
+                    <h1 onClick={this.handleVerce.bind(this)} className="ramtitle">{item.verse_title}</h1>
                     <div className="link-container">
                         <div className="outbox">
                             <a href="https://github.com/dlongs49" rel="noopener noreferrer" title="GitHub" target="_blank" className="item">
@@ -80,8 +80,8 @@ class Home extends Component {
                         <div className="mound_spade"></div>
                     </div>
                 </div>
-                <div className="fot">
-                    <span className="cr">Copyright 2019 - {new Date().getFullYear()} dinglong. All Rights Reserved</span>
+                <div className="footer">
+                    <span className="copyright">Copyright 2019 - {new Date().getFullYear()} dinglong. All Rights Reserved</span>
                     <a rel="noopener noreferrer" className="links" href="https://beian.miit.gov.cn/" target="_blank">陇ICP备2021000332号-1</a>
                 </div>
             </div>
