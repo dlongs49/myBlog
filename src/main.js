9c05c0290cfa5c8f2176c7c1961e655cc0797758
dinglong <dillonl49@163.com>
2024-03-06 11:22:25 +0800

    fix:#修改X的图标

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './assets/css/global.css'
 import App from './App.svelte'
 const app = new App({
-  target: document.getElementById('snow🌨'),
+  target: document.getElementById('🌨'),
 })
 
 export default app
